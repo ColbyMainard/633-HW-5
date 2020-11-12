130bbded4e3baeeb8063c6ff313a1066c2d8ecb5
ColbyMainard <55711813+ColbyMainard@users.noreply.github.com>
2020-11-12 10:26:22 -0600

    Added rough API
    Will likely need changes once a specific framework is decided on, but should provide useful structure

--- feature-extraction-image-preprocessing.py
+++ feature-extraction-image-preprocessing.py
@@ -5,15 +5,39 @@
 
 class ImagePreprocessor():
 	#image preprocessing implementation goes here
+	def __init__(self,directory_name):
+		self.data_directory = directory_name #directory containing data
+
+	def load_image(self, filename):
+		print("Returns an unprocessed image.")
+	
+	def process_image(image):
+		print("Returns a processed image.")
     
 class VisualFeatureExtractor():
 	#visual feature extraction implementation goes here
+	def __init__(self):
+		self.features_list = []
+		self.image_prepper = ImagePreprocessor()
+		print("Implement later.")
     
 class FeatureExplorer():
 	#feature exploration goes here
+	def __init__(self):
+		self.feature_extractor = VisualFeatureExtractor()
+		print("Implement later")
+	
+	def explore_features(self):
+		print("Do cursory examination on each feature found in VisualFeatureExtractor.")
 
 class FeatureSelector():
 	#feature selection goes here
+	def __init__(self):
+		self.feature_explorer = FeatureExplorer()
+		print("Implement later")
+	
+	def get_most_important_features():
+		print("Use this method to get most important features, as analyzed in feature explorer.")
 
 if __name__ == "__main__":
 	#add tests for each class below
