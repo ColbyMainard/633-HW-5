7efda5347d80f00432759e835a28dbf4907cf174
ColbyMainard <55711813+ColbyMainard@users.noreply.github.com>
2020-11-11 19:46:30 -0600

    Create feature-extraction-image-preprocessing.py

---
+++ feature-extraction-image-preprocessing.py
@@ -0,0 +1,23 @@
+# image-preprocessor.py
+# This file will contain all necessary methods and data to implement a.-b.iii.
+# This translates to image preprocessing, visual feature extraction, feature exploration, and feature selection
+# All the above functionality should be made available in methods/classes as to allow for easy access in other files
+
+class ImagePreprocessor():
+	#image preprocessing implementation goes here
+    
+class VisualFeatureExtractor():
+	#visual feature extraction implementation goes here
+    
+class FeatureExplorer():
+	#feature exploration goes here
+
+class FeatureSelector():
+	#feature selection goes here
+
+if __name__ == "__main__":
+	#add tests for each class below
+	print("Image preprocessing tests...")
+	print("Visual feature extraction tests...")
+	print("Feature exploration tests...")
+	print("Feature selection tests...")
