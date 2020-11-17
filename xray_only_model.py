967739cc1ca1ecc74e2201c085a787b1c963c182
ColbyMainard <55711813+ColbyMainard@users.noreply.github.com>
2020-11-17 17:10:08 -0600

    Add files via upload

--- xray_only_model.py
+++ xray_only_model.py
@@ -1,3 +1,4 @@
+from json import load
 from threading import current_thread
 from keras import models
 from numpy.core.defchararray import array
@@ -40,11 +41,30 @@ def hyperopt_space_xray_only():
 						cases_list.append((case_string, layer_count, optimizer, activators, epoch_num, layer_type))
 	return hp.choice('a', cases_list)
 
-def generate_model(layer_count, optimizer, activator_fun, layer_type):
+def percent_predictions_correct(y_expected, y_actual):
+	num = 0.0
+	denom = 0.0
+	for idx in range(0, len(y_expected)):
+		if y_expected[idx] == y_actual[idx]:
+			num += 1
+		denom += 1
+	return (num / denom)
+
+def format_images(directory_name, image_filename_list):
+	formatted_images = []
+	for image_filename in image_filename_list:
+		image_filename = os.path.join(directory_name, image_filename)
+		loaded_image = cv2.imread(image_filename, 0)
+		loaded_image = np.array(loaded_image)
+		loaded_image = (loaded_image / 255) - .5
+		formatted_images.append(loaded_image)
+	return formatted_images
+
+def get_cross_validation_accuracy(layer_count, optimizer, activator_fun, layer_type, epoch_count):
 	model = Sequential()
 	num_pixels = 600*600
 	denom_val = 1
-	model.add(Conv2D(32, kernel_size=(2,2), activation=activator_fun, input_shape=(600,600,0)))
+	model.add(Conv2D(32, kernel_size=3, activation=activator_fun, input_shape=(600,600,1)))
 	for idx in range (0, layer_count):
 		model.add(Conv2D(32, kernel_size=(1,1), activation=activator_fun))
 		if layer_type == Dense:
@@ -55,35 +75,20 @@ def generate_model(layer_count, optimizer, activator_fun, layer_type):
 		denom_val *= 2
 	model.add(Dense(2, activation='softmax'))
 	model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
-	return model
-
-def percent_predictions_correct(y_expected, y_actual):
-	num = 0.0
-	denom = 0.0
-	for idx in range(0, len(y_expected)):
-		if y_expected[idx] == y_actual[idx]:
-			num += 1
-		denom += 1
-	return (num / denom)
-
-def get_cross_validation_accuracy(model, epoch_count):
 	csv_x_data, csv_y_data = parse_csv_data("train.csv")
 	accuracy_list = []
 	image_filenames = csv_x_data[:,0]
 	folds = StratifiedKFold(n_splits=5, random_state=None, shuffle=False)
 	for train_index, test_index in folds.split(image_filenames, csv_y_data):
 		raw_x_train_data, raw_x_test_data = image_filenames[train_index], image_filenames[test_index]
-		x_train_data = []
-		for filename in raw_x_train_data:
-			filename = os.path.join("resized_train", filename)
-			x_train_data.append(cv2.imread(filename, 0))
+		x_train_data = format_images("resized_train", raw_x_train_data)
 		x_train_data = np.array(x_train_data)
-		print("Training data shape =", np.shape(x_train_data))
-		x_test_data = []
-		for filename in raw_x_test_data:
-			filename = os.path.join("resized_train", filename)
-			x_test_data.append(cv2.imread(filename, 0))
+		x_train_data.reshape(200, 600, 600, 1)
+		print("Training images shape:", str(x_train_data.shape))
+		x_test_data = format_images("resized_train", raw_x_test_data)
 		x_test_data = np.array(x_test_data)
+		x_test_data.reshape(50, 600, 600, 1)
+		print("Test images shape:", str(x_test_data.shape))
 		y_train_data, y_test_data = to_categorical(csv_y_data[train_index], num_classes=2), to_categorical(csv_y_data[test_index], num_classes=2)
 		model_history = model.fit(x_train_data, y_train_data, epochs=epoch_count, batch_size=200)
 		predictions = model.predict(x_test_data)
@@ -93,8 +98,7 @@ def get_cross_validation_accuracy(model, epoch_count):
 def find_optimal_parameters_xray_only(args):
 	case, layer_count, optimizer, activator_fun, epoch_count, layer_type = args
 	del case
-	test_model = generate_model(layer_count, optimizer, activator_fun, layer_type)
-	return 1 - get_cross_validation_accuracy(test_model, epoch_count)
+	return 1 - get_cross_validation_accuracy(layer_count, optimizer, activator_fun, layer_type, epoch_count)
 
 if __name__ == "__main__":
 	hyperopt_space = hyperopt_space_xray_only()
