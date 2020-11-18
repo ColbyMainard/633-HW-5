f739daa029bd6e8ef506e3ad85c58878f0a12d32
ColbyMainard <55711813+ColbyMainard@users.noreply.github.com>
2020-11-18 13:47:32 -0600

    Add files via upload

--- xray_only_model.py
+++ xray_only_model.py
@@ -7,7 +7,7 @@ from hyperopt import hp, fmin, tpe
 
 from keras import Sequential
 from keras.utils import to_categorical
-from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten
 from keras.models import model_from_json
 import numpy as np
 from numpy.lib.function_base import average
@@ -16,39 +16,10 @@ from sklearn.model_selection import StratifiedKFold
 from tensorflow.python.keras.engine import sequential
 
 import machine_learning_models
-from machine_learning_models import parse_csv_data
+from machine_learning_models import parse_csv_data, save_keras_model
 
 import os
 import cv2
-
-# x ray only neural network
-def hyperopt_space_xray_only():
-	possible_layer_counts = [3,4,5,6,7,8]
-	possible_compiler_optimizers = ['rmsprop', 'sgd']
-	activation_functions = ['relu', 'sigmoid']
-	epoch_count = range(1,10)
-	for count in epoch_count:
-		count *= 30
-	cases_list = []
-	case_number = 1
-	non_conv_layer_types = [Dense, MaxPooling2D]
-	for layer_count in possible_layer_counts:
-		for optimizer in possible_compiler_optimizers:
-			for activators in activation_functions:
-				for epoch_num in epoch_count:
-					for layer_type in non_conv_layer_types:
-						case_string = "case " + str(case_number)
-						cases_list.append((case_string, layer_count, optimizer, activators, epoch_num, layer_type))
-	return hp.choice('a', cases_list)
-
-def percent_predictions_correct(y_expected, y_actual):
-	num = 0.0
-	denom = 0.0
-	for idx in range(0, len(y_expected)):
-		if y_expected[idx] == y_actual[idx]:
-			num += 1
-		denom += 1
-	return (num / denom)
 
 def format_images(directory_name, image_filename_list):
 	formatted_images = []
@@ -62,47 +33,78 @@ def format_images(directory_name, image_filename_list):
 	formatted_images = np.array(formatted_images)
 	return formatted_images
 
-def get_cross_validation_accuracy(layer_count, optimizer, activator_fun, layer_type, epoch_count):
-	model = Sequential()
-	num_pixels = 600*600
-	denom_val = 1
-	model.add(Conv2D(32, kernel_size=3, activation=activator_fun, input_shape=(None,600,600,1)))
-	for idx in range (0, layer_count):
-		model.add(Conv2D(32, kernel_size=(1,1), activation=activator_fun))
-		if layer_type == Dense:
-			model.add(Dense(num_pixels // denom_val, activation=activator_fun))
-		if layer_type == MaxPooling2D:
-			model.add(MaxPooling2D(pool_size=(2,2)))
-		model.add(Dropout(0.1))
-		denom_val *= 2
-	model.add(Dense(2, activation='softmax'))
-	model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+def accuracy_percent(y_expected, y_actual):
+	num = 0.0
+	denom = 0.0
+	for idx in len(y_expected):
+		if(y_expected[idx] == y_actual[idx]):
+			num += 1
+		denom += 1
+	return num / denom
+
+def get_average_cross_validation_accuracy(args):
+	case_string, conv_layer_count, dense_layer_count, compiler_optimizer, active_fun = args
 	csv_x_data, csv_y_data = parse_csv_data("train.csv")
-	accuracy_list = []
+	feature = []
+	print("Case:", case_string)
+	if(conv_layer_count == 3):
+		feature = [Conv2D(4, kernel_size=3, activation=active_fun, input_shape=(600,600,1)), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Flatten(),]
+	elif(conv_layer_count == 4):
+		feature = [Conv2D(4, kernel_size=3, activation=active_fun, input_shape=(600,600,1)), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Flatten(),]
+	elif(conv_layer_count == 5):
+		feature = [Conv2D(4, kernel_size=3, activation=active_fun, input_shape=(600,600,1)), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Flatten(),]
+	elif(conv_layer_count == 6):
+		feature = [Conv2D(4, kernel_size=3, activation=active_fun, input_shape=(600,600,1)), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Flatten(),]
+	elif(conv_layer_count == 7):
+		feature = [Conv2D(4, kernel_size=3, activation=active_fun, input_shape=(600,600,1)), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Flatten(),]
+	classifier = []
+	if dense_layer_count == 2:
+		classifier = [Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(2, activation='softmax'),]
+	elif dense_layer_count == 3:
+		classifier = [Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(2, activation='softmax'),]
+	elif dense_layer_count == 4:
+		classifier = [Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(2, activation='softmax'),]
+	elif dense_layer_count == 5:
+		classifier = [Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(2, activation='softmax'),]
+	elif dense_layer_count == 6:
+		classifier = [Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(2, activation='softmax'),]
+	elif dense_layer_count == 6:
+		classifier = [Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(32, activation=active_fun), Dense(2, activation='softmax'),]
 	image_filenames = csv_x_data[:,0]
+	accuracy_vals = []
 	folds = StratifiedKFold(n_splits=5, random_state=None, shuffle=False)
 	for train_index, test_index in folds.split(image_filenames, csv_y_data):
 		raw_x_train_data, raw_x_test_data = image_filenames[train_index], image_filenames[test_index]
 		x_train_data = format_images("resized_train", raw_x_train_data)
-		#x_train_data = np.array(x_train_data)
 		x_train_data = x_train_data.reshape(200, 600, 600, 1)
 		print("Training images shape:", str(x_train_data.shape))
 		x_test_data = format_images("resized_train", raw_x_test_data)
-		#x_test_data = np.array(x_test_data)
 		x_test_data = x_test_data.reshape(50, 600, 600, 1)
 		print("Test images shape:", str(x_test_data.shape))
 		y_train_data, y_test_data = to_categorical(csv_y_data[train_index], num_classes=2), to_categorical(csv_y_data[test_index], num_classes=2)
-		model_history = model.fit(x_train_data, y_train_data, epochs=epoch_count, batch_size=200)
-		predictions = model.predict(x_test_data)
-		accuracy_list.append(percent_predictions_correct(predictions, y_test_data))
-	return 1 - average(accuracy_list)
+		model = Sequential(feature+classifier)
+		model.compile(optimizer=compiler_optimizer, loss='categorical_crossentropy',metrics=['accuracy'],)
+		model.fit(x_train_data, y_train_data,epochs=20,batch_size=25)
+		accuracy_vals.append(accuracy_percent(model.predict(x_test_data), y_test_data))
+	return 1 - average(accuracy_vals)
 
-def find_optimal_parameters_xray_only(args):
-	case, layer_count, optimizer, activator_fun, epoch_count, layer_type = args
-	del case
-	return 1 - get_cross_validation_accuracy(layer_count, optimizer, activator_fun, layer_type, epoch_count)
-
-if __name__ == "__main__":
-	hyperopt_space = hyperopt_space_xray_only()
-	best_parameters = fmin(find_optimal_parameters_xray_only, hyperopt_space, algo=tpe.suggest, max_evals=5000)
+def optimize_hyperparameters():
+	possible_conv_layer_counts = [3,4,5,6,7]
+	possible_dense_layer_counts = [2,3,4,5,6,7]
+	possible_compiler_optimizers = ['rmsprop', 'sgd']
+	activation_functions = ['relu', 'sigmoid']
+	case_list = []
+	case_num = 1
+	for conv_layer_count in possible_conv_layer_counts:
+		for dense_layer_count in possible_dense_layer_counts:
+			for compiler_optimizer in possible_compiler_optimizers:
+				for active_fun in activation_functions:
+					case_string = "case " + str(case_num)
+					case_num += 1
+					case_list.append((case_string, conv_layer_count, dense_layer_count, compiler_optimizer, active_fun))
+	feature_space = hp.choice('a', case_list)
+	best_parameters = fmin(get_average_cross_validation_accuracy, feature_space, algo=tpe.suggest, max_evals=250)
 	print("Best parameters:", best_parameters)
+	
+if __name__ == "__main__":
+        optimize_hyperparameters()
