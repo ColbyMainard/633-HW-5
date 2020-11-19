cef4524f267cf60f039f81c81ed68ab48b4e092c
[Minh <minhdangtn@tamu.edu>
2020-11-19 00:24:16 -0600

    update get label method

--- feature_selection.py
+++ feature_selection.py
@@ -9,16 +9,17 @@ from sklearn.feature_selection import mutual_info_classif
 from sklearn.decomposition import PCA
 from feature_extraction_image_preprocessing import *
 
+
 def wrapperFeatureSelect(x_train, y_train):
     model = LogisticRegression()
 
     rfe_engine = RFE(model, 5)
     rfe_result = rfe_engine.fit(x_train, y_train)
-    
-    print("Num Features: %s"        % (rfe_result.n_features_))
-    print("Selected Features: %s"   % (rfe_result.support_))
-    print("Feature Ranking: %s"     % (rfe_result.ranking_))
-    
+
+    print("Num Features: %s" % rfe_result.n_features_)
+    print("Selected Features: %s" % rfe_result.support_)
+    print("Feature Ranking: %s" % rfe_result.ranking_)
+
 
 def filterFeatureSelect(x_train, y_train):
     test = SelectKBest(score_func=mutual_info_classif, k=5)
@@ -31,7 +32,7 @@ def filterFeatureSelect(x_train, y_train):
     features = filter_result.transform(x_train)
 
     # Summarize selected features
-    print(features[0:5,:])
+    print(features[0:5, :])
 
 
 def reduceFeatures(x_train):
@@ -41,29 +42,27 @@ def reduceFeatures(x_train):
 
 
 if __name__ == "__main__":
-    y_data          = np.load('train_data.npy')
-    x_gray_scale    = getFeature("gray_scale")
-    x_mean_pixel    = getFeature("mean_pixel")
+    y_data = np.load('train_data.npy')
+    x_gray_scale = getFeature("gray_scale")
+    x_mean_pixel = getFeature("mean_pixel")
     x_edge_vertical = getFeature("extracting_edge_vertical")
-    x_edge_horizon  = getFeature("extracting_edge_horizontal")
-    x_hog           = getFeature("hog")
+    x_edge_horizon = getFeature("extracting_edge_horizontal")
+    x_hog = getFeature("hog")
 
     x_train = []
     y_train = []
 
-    print("Combining image features...",end="")
+    print("Combining image features...", end="")
     for index in range(len(x_gray_scale)):
-        x_train.append(np.concatenate((x_gray_scale[index][1],    \
-                                       x_mean_pixel[index][1],    \
-                                       x_edge_horizon[index][1],  \
-                                       x_edge_vertical[index][1], \
+        x_train.append(np.concatenate((x_gray_scale[index][1],
+                                       x_mean_pixel[index][1],
+                                       x_edge_horizon[index][1],
+                                       x_edge_vertical[index][1],
                                        x_hog[index][1])))
-        for y_index in range(len(y_data)):
-            if x_gray_scale[index][0] == y_data[y_index][0]:
-                y_train.append(int(y_data[y_index][4]))
+        y_train.append(x_gray_scale[index][0])
     print("done")
 
-    print("Reducing image features with PCA...",end="")
+    print("Reducing image features with PCA...")
     principalComponents = reduceFeatures(x_train)
     print("done")
     x_train = np.array(principalComponents)
@@ -73,6 +72,6 @@ if __name__ == "__main__":
     # wrapperFeatureSelect(x_train, y_train)
     # print("done")
 
-    print("Selecting features with LinearDiscriminantAnalysis (filter)...",end="")
+    print("Selecting features with LinearDiscriminantAnalysis (filter)...", end="")
     filterFeatureSelect(x_train, y_train)
     print("done")
