982dcc93139172de845c030acfa594df7318dfe5
ColbyMainard <55711813+ColbyMainard@users.noreply.github.com>
2020-11-21 14:02:51 -0600

    Add files via upload

--- xray_only_model.py
+++ xray_only_model.py
@@ -34,8 +34,10 @@ def format_images(directory_name, image_filename_list):
 	return formatted_images
 
 def accuracy_percent(y_expected, y_actual):
-	y_expected = y_expected.reshape(50, 2)
-	y_actual = y_actual.reshape(50, 2)
+	#y_expected = y_expected.reshape(50, 2)
+	y_expected = y_expected.reshape(50, 1)
+	#y_actual = y_actual.reshape(50, 2)
+	y_actual = y_actual.reshape(50, 1)
 	num = 0.0
 	denom = 50.0
 	y_diff = (y_actual - y_expected)
@@ -67,19 +69,19 @@ def get_average_cross_validation_accuracy(args):
 			feature = [Conv2D(4, kernel_size=3, activation=active_fun, input_shape=(600,600,1)), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Flatten(),]
 		classifier = []
 		if dense_layer_count == 2:
-			classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='softmax'),]
+			classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='sigmoid'),]
 		elif dense_layer_count == 3:
-			classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(2, activation='softmax'),]
+			classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='sigmoid'),]
 		elif dense_layer_count == 4:
-			classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(2, activation='softmax'),]
+			classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='sigmoid'),]
 		elif dense_layer_count == 5:
-			classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(2, activation='softmax'),]
+			classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='sigmoid'),]
 		elif dense_layer_count == 6:
-			classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(2, activation='softmax'),]
+			classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='sigmoid'),]
 		elif dense_layer_count == 6:
-			classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(2, activation='softmax'),]
+			classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='sigmoid'),]
 		elif dense_layer_count == 7:
-			classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(2, activation='softmax'),]
+			classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='sigmoid'),]
 		image_filenames = csv_x_data[:,0]
 		accuracy_vals = []
 		folds = StratifiedKFold(n_splits=5, random_state=None, shuffle=False)
@@ -87,17 +89,16 @@ def get_average_cross_validation_accuracy(args):
 			raw_x_train_data, raw_x_test_data = image_filenames[train_index], image_filenames[test_index]
 			x_train_data = format_images("resized_train", raw_x_train_data)
 			x_train_data = x_train_data.reshape(200, 600, 600, 1)
-		#print("Training images shape:", str(x_train_data.shape))
 			x_test_data = format_images("resized_train", raw_x_test_data)
 			x_test_data = x_test_data.reshape(50, 600, 600, 1)
-		#print("Test images shape:", str(x_test_data.shape))
 			y_train_data, y_test_data = csv_y_data[train_index], csv_y_data[test_index]
 			model = Sequential(feature+classifier)
 			model.compile(optimizer=compiler_optimizer, loss='binary_crossentropy',metrics=['accuracy'],)
-		#model.fit(x_train_data, y_train_data,epochs=5,batch_size=5, verbose=0)
-			model.fit(x_train_data, to_categorical(y_train_data), epochs=10, batch_size=5, verbose=0)
+			model.fit(x_train_data, y_train_data, epochs=10, batch_size=5, verbose=0)
+			#model.fit(x_train_data, to_categorical(y_train_data), epochs=10, batch_size=5, verbose=0)
 			y_expected = model.predict(x_test_data)
-			accuracy_vals.append(accuracy_percent(y_expected, to_categorical(y_test_data)))
+			#accuracy_vals.append(accuracy_percent(y_expected, to_categorical(y_test_data)))
+			accuracy_vals.append(accuracy_percent(y_expected, y_test_data))
 		print("Accuracy vals: ", accuracy_vals)
 		correctness = average(accuracy_vals)
 		print("Average percent correct:", correctness)
@@ -121,7 +122,6 @@ def optimize_hyperparameters():
 		for dense_layer_count in possible_dense_layer_counts:
 			for compiler_optimizer in possible_compiler_optimizers:
 				for active_fun in activation_functions:
-					case_string = "case " + str(case_num)
 					case_num += 1
 					case_list.append((conv_layer_count, dense_layer_count, compiler_optimizer, active_fun))
 	feature_space = hp.choice('a', case_list)
@@ -143,26 +143,27 @@ def implement_optimum_model(parameter_tuple):
 		feature = [Conv2D(4, kernel_size=3, activation=active_fun, input_shape=(600,600,1)), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Conv2D(4, kernel_size=3, activation=active_fun), MaxPooling2D(pool_size=2), Flatten(),]
 	classifier = []
 	if dense_layer_count == 2:
-		classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='softmax'),]
+		classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='sigmoid'),]
 	elif dense_layer_count == 3:
-		classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(2, activation='softmax'),]
+		classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='sigmoid'),]
 	elif dense_layer_count == 4:
-		classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(2, activation='softmax'),]
+		classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='sigmoid'),]
 	elif dense_layer_count == 5:
-		classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(2, activation='softmax'),]
+		classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='sigmoid'),]
 	elif dense_layer_count == 6:
-		classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(2, activation='softmax'),]
+		classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='sigmoid'),]
 	elif dense_layer_count == 6:
-		classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(2, activation='softmax'),]
+		classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='sigmoid'),]
 	elif dense_layer_count == 7:
-		classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(2, activation='softmax'),]
+		classifier = [Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(32, activation=active_fun), Dropout(0.1), Dense(1, activation='sigmoid'),]
 	raw_x_data = csv_x_data[:,0]
 	raw_x_data =  format_images("resized_train", raw_x_data)
 	x_data = raw_x_data.reshape(250, 600, 600, 1)
 	y_data = csv_y_data
 	model = Sequential(feature+classifier)
 	model.compile(optimizer=compiler_optimizer, loss='binary_crossentropy',metrics=['accuracy'],)
-	model_history = model.fit(x_data, to_categorical(y_data), epochs=12, batch_size=5)
+	#model_history = model.fit(x_data, to_categorical(y_data), epochs=10, batch_size=5)
+	model_history = model.fit(x_data, y_data, epochs=10, batch_size=5)
 	print("Accuracy history:", model_history.history['accuracy'])
 	save_keras_model(model, "optimum_xray_model.json","optimum_xray_model.h5")
 
@@ -173,3 +174,8 @@ if __name__ == "__main__":
 	model = load_keras_model("optimum_xray_model.json","optimum_xray_model.h5")
 	print("Model summary:")
 	print(model.summary())
+	csv_x_data, csv_y_data = parse_csv_data("test.csv", False)
+	raw_x_data = csv_x_data[:,0]
+	raw_x_data = format_images("resized_test", raw_x_data)
+	x_data = raw_x_data.reshape(94,600,600,1)
+	model.predict(x_data)
